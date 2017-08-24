13d5952439e010d42fff8f3360d5564634cf5979
福さん <ikishikawa64@gmail.com>
2017-08-25 01:42:18 +0900

    複数台に文字列を送れるようになった

--- Siv3D(August2016v2)1/Main.cpp
+++ Siv3D(August2016v2)1/Main.cpp
@@ -8,14 +8,20 @@ void Main()
 	int req;
 	std::string res("114514\n");
 	Window::SetTitle(L"サーバー");
-	server.startAccept(80);
+	bool isOpen = false;
 	while (System::Update())
 	{
+		if (!isOpen) {
+			server.startAccept(80);
+			isOpen = true;
+		}
 		if (server.isConnected()) {
 			while (server.read(req));
-			font(req).draw();
-			server.send(res.c_str(),sizeof(char) * (res.length() + 1));
-
+			Println(L"req:",req);
+			server.send(res.c_str(), sizeof(char) * (res.length() + 1));
+			Println(L"send:114514");
+			server.disconnect();
+			isOpen = false;
 		}
 
 	}
