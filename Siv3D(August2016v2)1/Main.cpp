59c350d75c8d4b4839e24319438d2a1a2f0cd00b
福さん <ikishikawa64@gmail.com>
2017-08-26 17:35:47 +0900

    とりあえず上げる

--- Siv3D(August2016v2)1/Main.cpp
+++ Siv3D(August2016v2)1/Main.cpp
@@ -1,28 +1,56 @@
 ﻿# include <string>
 # include <Siv3D.hpp>
 
+class Server{
+	private:
+		TCPServer tcp_server;
+		std::string function_cnt;
+		bool is_open;
+		std::string read_req(void) {
+			std::string message;
+			while(true){
+				char character;
+
+				if (!tcp_server.read(character)) {
+					return "";
+				}
+
+				if (character == '\0'){
+					return message;
+				}else{
+					message.push_back(character);
+				}
+			}
+		}
+	public:
+		Server(int functionCount) {
+			function_cnt = std::to_string(functionCount);
+			is_open = false;
+		}
+		std::string update(void) {
+			std::string message;
+			if (!is_open) {
+				tcp_server.startAccept(80);
+				is_open = true;
+			}
+			if (tcp_server.isConnected()) {
+				message = read_req();
+				Println(L"req:", FromUTF8(message));
+				if (message == "get_list") {
+					tcp_server.send(function_cnt.c_str(), sizeof(char) * (function_cnt.length() + 1));
+					return "";
+				}
+				tcp_server.disconnect();
+				is_open = false;
+				return message;
+			}
+			return "";
+		}
+};
 void Main()
 {
-	const Font font(30);
-	TCPServer server;
-	int req;
-	std::string res("114514\n");
-	Window::SetTitle(L"サーバー");
-	bool isOpen = false;
-	while (System::Update())
-	{
-		if (!isOpen) {
-			server.startAccept(80);
-			isOpen = true;
-		}
-		if (server.isConnected()) {
-			while (server.read(req));
-			Println(L"req:",req);
-			server.send(res.c_str(), sizeof(char) * (res.length() + 1));
-			Println(L"send:114514");
-			server.disconnect();
-			isOpen = false;
-		}
-
+	Server server(5);
+	while (System::Update()) {
+		server.update();
 	}
 }
