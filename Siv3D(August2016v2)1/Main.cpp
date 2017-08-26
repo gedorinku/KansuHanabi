dcf55badeb46c7a5666d5237da8eaef30581c383
福さん <ikishikawa64@gmail.com>
2017-08-26 22:15:12 +0900

    動かない

--- Siv3D(August2016v2)1/Main.cpp
+++ Siv3D(August2016v2)1/Main.cpp
@@ -1,17 +1,21 @@
 ﻿# include <string>
 # include <Siv3D.hpp>
+# include <cassert>
+#include <thread>
+# include "BlockingQueue.h"
 
 class Server{
 	private:
 		TCPServer tcp_server;
 		std::string function_cnt;
-		bool is_open;
+		blocking_queue<std::string> request_queue;
 		std::string read_req(void) {
 			std::string message;
 			while(true){
 				char character;
 
 				if (!tcp_server.read(character)) {
+					Println(L"read error");
 					return "";
 				}
 
@@ -22,35 +26,43 @@ class Server{
 				}
 			}
 		}
-	public:
-		Server(int functionCount) {
-			function_cnt = std::to_string(functionCount);
-			is_open = false;
-		}
-		std::string update(void) {
-			std::string message;
-			if (!is_open) {
-				tcp_server.startAccept(80);
-				is_open = true;
-			}
+		void update(void) {
 			if (tcp_server.isConnected()) {
+				std::string message;
 				message = read_req();
-				Println(L"req:", FromUTF8(message));
+				Println(L"message:", FromUTF8(message));
 				if (message == "get_list") {
 					tcp_server.send(function_cnt.c_str(), sizeof(char) * (function_cnt.length() + 1));
-					return "";
+					Println(L"res", FromUTF8(function_cnt));
+				}
+				else {
+					request_queue.enqueue(message);
 				}
 				tcp_server.disconnect();
-				is_open = false;
-				return message;
+				tcp_server.startAccept(80);
+				return;
 			}
-			return "";
+			
+			
+		}
+	public:
+		
+		Server(int functionCount) {
+			function_cnt = std::to_string(functionCount);
+		}
+		void start() {
+			std::thread s([&] {
+				tcp_server.startAccept(80);
+				while (true) {
+					update();
+				}
+			});
+			s.detach();
 		}
 };
 void Main()
 {
 	Server server(5);
-	while (System::Update()) {
-		server.update();
-	}
+	server.start();
+	while (System::Update());
 }
